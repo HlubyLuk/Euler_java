f12c6a9e5dd633fd16ac6422f5e17e883c912eba
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2016-12-18 10:16:39 +0100

    Test problem 33

---
+++ src/test/java/cz/hlubyluk/euler/problems/Problem33Test.java
@@ -0,0 +1,14 @@
+package cz.hlubyluk.euler.problems;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by HlubyLuk on 18/12/2016.
+ */
+public class Problem33Test {
+    @Test
+    public void solve() throws Exception {
+        Assert.assertEquals(100, new Problem33().solve());
+    }
+}
