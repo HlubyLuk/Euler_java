357589f33814926f4f7678d82852ecf449166258
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2017-11-12 20:45:31 +0100

    Add missing tests

---
+++ src/test/java/cz/hlubyluk/euler/problems/Problem35Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.hlubyluk.euler.problems;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author HlubyLuk
+ */
+public class Problem35Test {
+
+    @Test
+    public void problem35() throws Exception {
+        Assert.assertEquals(289L, new Problem35().solve());
+    }
+}
---
+++ src/test/java/cz/hlubyluk/euler/problems/Problem36Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.hlubyluk.euler.problems;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author HlubyLuk
+ */
+public class Problem36Test {
+
+    @Test
+    public void problem36() throws Exception {
+        Assert.assertEquals(872187, new Problem36().solve());
+    }
+}
