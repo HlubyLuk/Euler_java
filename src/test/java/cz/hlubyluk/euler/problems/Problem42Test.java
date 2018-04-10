73f28ca86e2043e2f8d663fec8c82aaa6d30aa2d
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2018-04-10 21:31:24 +0200

    Problem42

---
+++ src/test/java/cz/hlubyluk/euler/problems/Problem42Test.java
@@ -0,0 +1,14 @@
+package cz.hlubyluk.euler.problems;
+
+public class Problem42Test extends ProblemAbsTest<Problem42> {
+
+	@Override
+	protected Problem42 getInstance() {
+		return new Problem42();
+	}
+
+	@Override
+	protected Number getResult() {
+		return 162;
+	}
+}
