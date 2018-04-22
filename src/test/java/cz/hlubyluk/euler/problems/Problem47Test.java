99af7a94b3badec7436bb1fbc9b319cbbbe8194b
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2018-04-22 11:07:25 +0200

    Problem47

---
+++ src/test/java/cz/hlubyluk/euler/problems/Problem47Test.java
@@ -0,0 +1,15 @@
+package cz.hlubyluk.euler.problems;
+
+public class Problem47Test extends ProblemAbsTest<Problem47> {
+
+	@Override
+	protected Problem47 getInstance() {
+		return new Problem47();
+	}
+
+	@Override
+	protected Number getResult() {
+		return 134043;
+	}
+
+}
